55caa555551eea76c4bf582efe1ce32df0d3d1b4
Geuni <dlrmsgnl620@naver.com>
2024-03-27 00:57:59 +0900

    부모가 렌더링되면 props 여부에 상관없이 자식도 렌더링 된다

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import { Button } from 'src/components/ui/button';
+
+export const App = () => {
+  console.log('Rendering App');
+  return (
+    <div style={{ border: '1px solid black', padding: '10px' }}>
+      App
+      <Component1 />
+    </div>
+  );
+};
+
 const Component1 = () => {
   console.log('Rendering Component 1');
   return (
-    <div>
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 1
       <Component2 />
       <Component3 />
     </div>
@@ -9,9 +23,14 @@ const Component1 = () => {
 };
 
 const Component2 = () => {
+  const [count, setCount] = useState(0);
   console.log('Rendering Component 2');
   return (
-    <div>
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 2
+      <Button className="ml-5" onClick={() => setCount(count + 1)}>
+        Rerender Component 2 (Count: {count})
+      </Button>
       <Component4 />
       <Component5 />
     </div>
@@ -20,13 +39,18 @@ const Component2 = () => {
 
 const Component3 = () => {
   console.log('Rendering Component 3');
-  return <div>Component 3</div>;
+  return (
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 3
+    </div>
+  );
 };
 
 const Component4 = () => {
   console.log('Rendering Component 4');
   return (
-    <div>
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 4
       <Component6 />
       <Component7 />
     </div>
@@ -36,7 +60,8 @@ const Component4 = () => {
 const Component5 = () => {
   console.log('Rendering Component 5');
   return (
-    <div>
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 5
       <Component8 />
       <Component9 />
     </div>
@@ -45,29 +70,36 @@ const Component5 = () => {
 
 const Component6 = () => {
   console.log('Rendering Component 6');
-  return <div>Component 6</div>;
+  return (
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 6
+    </div>
+  );
 };
 
 const Component7 = () => {
   console.log('Rendering Component 7');
-  return <div>Component 7</div>;
+  return (
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 7
+    </div>
+  );
 };
 
 const Component8 = () => {
   console.log('Rendering Component 8');
-  return <div>Component 8</div>;
+  return (
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 8
+    </div>
+  );
 };
 
 const Component9 = () => {
   console.log('Rendering Component 9');
-  return <div>Component 9</div>;
-};
-
-export const App = () => {
-  console.log('Rendering App');
   return (
-    <div>
-      <Component1 />
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+      Component 9
     </div>
   );
 };
